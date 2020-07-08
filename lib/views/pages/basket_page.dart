7bc8c499c15e2061003f7bf43f0855044d4163d0
Falynsky <kamil.falinski@gmail.com>
2020-07-08 23:02:46 +0200

    Handling retrieving products from user's basket

--- lib/views/pages/basket_page.dart
+++ lib/views/pages/basket_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smartmarktclient/bloc/bloc.dart';
+import 'package:smartmarktclient/http/http_service.dart';
 
 class BasketPage extends StatefulWidget {
   @override
@@ -9,9 +10,12 @@ class BasketPage extends StatefulWidget {
 
 class _BasketPageState extends State<BasketPage> {
   RouteBloc _routeBloc;
+  HttpService _httpService;
 
   @override
   void initState() {
+    _httpService = HttpService();
+    test();
     _routeBloc = BlocProvider.of<RouteBloc>(context);
     super.initState();
   }
@@ -43,5 +47,24 @@ class _BasketPageState extends State<BasketPage> {
         color: Colors.amber,
       ),
     );
+  }
+
+  void test() async {
+    String basketUrl = "/baskets_products/getUserProducts";
+    final response = await _httpService.get(url: basketUrl);
+
+    if (response['success'] == true) {
+      List list = response['data'];
+      list.forEach((element) {
+        print(element['name'] +
+            " : " +
+            element['quantity'].toString() +
+            " " +
+            element['quantityType']);
+      });
+      print("Retrieved data products");
+    } else {
+      print("Error while retrieving basket products");
+    }
   }
 }
